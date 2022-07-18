e9dfb470021424cb56aa464adca3811a14abd0ea
JavidPack <javidpack@gmail.com>
2022-07-18 17:05:25 -0600

    v1.2.2, add warning if corruption spread is enabled

---
+++ DisableCorruptionSpreadPlayer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DisableCorruptionSpread
+{
+	public class DisableCorruptionSpreadPlayer : ModPlayer
+	{
+		public override void OnEnterWorld(Player player) {
+			if (!DisableCorruptionSpreadModWorld.CorruptionSpreadDisabled) {
+				Main.NewText("Warning: Corruption Spread is enabled. Corruption will spread as normal. Use /spread in chat to disable spreading again.", Color.Orange);
+			}
+			//Mod.Logger.Info($"OnEnterWorld, CorruptionSpreadDisabled: {DisableCorruptionSpreadModWorld.CorruptionSpreadDisabled}");
+		}
+	}
+}
